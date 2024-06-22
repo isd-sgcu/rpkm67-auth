9d766eb02cbd606fc007ba8a567b2cc0378c3676
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-22 21:17:49 +0700

    config redis

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -16,9 +17,25 @@ type DbConfig struct {
 	Url string
 }
 
+type RedisConfig struct {
+	Host     string
+	Port     int
+	Password string
+}
+
+type JwtConfig struct {
+	Secret        string
+	AccessTTL     int
+	RefreshTTL    int
+	Issuer        string
+	ResetTokenTTL int
+}
+
 type Config struct {
-	App AppConfig
-	Db  DbConfig
+	App   AppConfig
+	Db    DbConfig
+	Redis RedisConfig
+	Jwt   JwtConfig
 }
 
 func LoadConfig() (*Config, error) {
@@ -39,9 +56,43 @@ func LoadConfig() (*Config, error) {
 		Url: os.Getenv("DB_URL"),
 	}
 
+	redisPort, err := strconv.ParseInt(os.Getenv("REDIS_PORT"), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	redisConfig := RedisConfig{
+		Host:     os.Getenv("REDIS_HOST"),
+		Port:     int(redisPort),
+		Password: os.Getenv("REDIS_PASSWORD"),
+	}
+
+	accessTTL, err := strconv.ParseInt(os.Getenv("JWT_ACCESS_TTL"), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	refreshTTL, err := strconv.ParseInt(os.Getenv("JWT_REFRESH_TTL"), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	resetTokenTTL, err := strconv.ParseInt(os.Getenv("JWT_RESET_TOKEN_TTL"), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	jwtConfig := JwtConfig{
+		Secret:        os.Getenv("JWT_SECRET"),
+		AccessTTL:     int(accessTTL),
+		RefreshTTL:    int(refreshTTL),
+		Issuer:        os.Getenv("JWT_ISSUER"),
+		ResetTokenTTL: int(resetTokenTTL),
+	}
+
 	return &Config{
-		App: appConfig,
-		Db:  dbConfig,
+		App:   appConfig,
+		Db:    dbConfig,
+		Redis: redisConfig,
+		Jwt:   jwtConfig,
 	}, nil
 }
 
